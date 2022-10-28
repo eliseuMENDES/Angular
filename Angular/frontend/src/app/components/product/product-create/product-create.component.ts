72337d74dce3134d686a86ccdd7e0a4b8e836c18
Eliseu <eliseu839@gmail.com>
2022-10-28 18:51:38 -0300

    Add files via upload

---
+++ Angular/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,37 @@
+import { Product } from '../product.model'
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: 'product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name: 'Produto de Teste',
+    price: 125.98
+  }
+
+  constructor(private productService: ProductService,
+    private router: Router) { }
+
+
+  ngOnInit(): void {
+
+  }
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showOnConsole('Produto criado!')
+      this.router.navigate(['/products'])       
+    })
+
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+}
